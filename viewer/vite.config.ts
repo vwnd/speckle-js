1fc774da5f0cfa372a6236eb01d821306e1e42e1
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-11-23 12:46:41 +0100

    push?

--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -1,20 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import vue from '@vitejs/plugin-vue'
-import autoprefixer from 'autoprefixer'
-import tailwind from 'tailwindcss'
-import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/<REPO>/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   css: {
     postcss: { plugins: [autoprefixer(), tailwind()] },
   },
-})
+});
