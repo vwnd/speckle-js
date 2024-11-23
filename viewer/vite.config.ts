12154fb69fecb6ab83f4c9a9ea8d77b6e7efbcff
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-11-23 12:59:56 +0100

    another try with base

--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -8,7 +8,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://vwnd.github.io/speckle-js/",
+  base: "/speckle-js/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
