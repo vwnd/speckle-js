f454edfb38dea8f632ea0611b43799a4b69e23a7
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-11-23 11:58:58 +0100

    feat: page title

--- viewer/src/router/index.ts
+++ viewer/src/router/index.ts
@@ -1,15 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
+      meta: {
+        title: "Speckle.js",
+      },
     },
   ],
-})
+});
 
-export default router
+router.beforeEach((to) => {
+  document.title = (to.meta?.title as string | undefined) || "Speckle.js";
+});
+
+export default router;
