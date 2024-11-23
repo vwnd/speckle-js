5af87280f98379880f7520b52a248a4bea517c4d
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-11-23 12:38:13 +0100

    feat: camera screenshot

--- viewer/src/views/HomeView.vue
+++ viewer/src/views/HomeView.vue
@@ -2,12 +2,32 @@
 import ModelInput from "@/components/ModelInput.vue";
 import ModelViewer from "@/components/ModelViewer.vue";
 import { Button } from "@/components/ui/button";
+import { CameraIcon } from "lucide-vue-next";
 import { ref } from "vue";
+import html2canvas from "html2canvas";
 
 const model = ref<string | null>(null);
 
 const clearModel = () => {
   model.value = null;
+};
+
+const captureScreenshot = async () => {
+  const buttons = document.querySelectorAll(".screenshot-hide");
+  buttons.forEach((button) => (button.style.display = "none"));
+
+  const element = document.querySelector("main");
+  if (!element) return;
+
+  const canvas = await html2canvas(element);
+  const dataUrl = canvas.toDataURL("image/png");
+
+  const link = document.createElement("a");
+  link.href = dataUrl;
+  link.download = "screenshot.png";
+  link.click();
+
+  buttons.forEach((button) => (button.style.display = ""));
 };
 </script>
 
@@ -19,9 +39,17 @@ const clearModel = () => {
       variant="outline"
       v-if="model"
       @click="model = null"
-      class="absolute top-4 right-4 z-10"
+      class="absolute top-4 right-4 z-10 screenshot-hide"
       >X</Button
     >
+    <Button
+      variant="outline"
+      v-if="model"
+      @click="captureScreenshot"
+      class="absolute top-4 right-16 z-10 screenshot-hide"
+      ><CameraIcon
+    /></Button>
+
     <ModelViewer
       v-if="model"
       :model="model"
